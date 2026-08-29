156e26280e36a1342fa2eeda3ba2b8d03e9baa09
BKerio <briankerio48@gmail.com>
2026-08-29 21:34:49 +0300

    new updates

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -8,7 +8,7 @@ const client = axios.create({
 client.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
   if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+  return config;YYY
 });
 
 client.interceptors.response.use(
